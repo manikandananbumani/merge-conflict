cf17137c266c88183ef76ba8ef8e358532c9891c
devops-intellipaat <hemant@intellipaat.com>
2019-01-18 02:33:59 +0000

    added feature2

--- main.c
+++ main.c
@@ -5,11 +5,18 @@ Initial Function()
   //Sample Code
 }
 
+<<<<<<< HEAD
 Initial Function2()
 {
  //Sample Code
 }
 
+=======
+feaature2()
+{
+  //Sample Code for Feature 2
+}
+>>>>>>> added feature2
 
 
 }
