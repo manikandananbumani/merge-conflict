0ef4e914875497bf27857edb230166c69568fa0e
Mani <manikandanece43@gmail.com>
2019-10-06 15:53:45 +0530

    added feature1 with patch

--- main.c
+++ main.c
@@ -5,15 +5,17 @@ Initial Function()
   //Sample Code
 }
 
-<<<<<<< HEAD
+
 Initial Function2()
 {
  //Sample Code
-=======
+}
+
+
 feature1()
 {
   //Sample Code for Feature 1
->>>>>>> added feature A
+
 }
 
 
